81f0b38740041bb7f5e69f75718121bd3e0ec3b1
Rory Goddard <goddardrory@gmail.com>
2024-09-21 12:52:57 +0200

    Feat: Add dispatch method and comments

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ function newStore(reducer) {
     let state = 0;
     let subscribers = [];
 
+    // Get the state
     const getState = function () {
         return state;
     };
 
+    // Add a subscriber function to the subscribers array
     const subscribe = function (subscriber) {
         subscribers.push(subscriber);
     };
 
+    // Remove a subscriber function from the subscribers array
     const unsubscribe = function (subscriber) {
-        subscribers.filter(item => item !== subscriber)
+        subscribers.filter(item => item !== subscriber);
+    };
+
+    // Dispatch an action to modify the state
+    const dispatch = function (action) {
+        state = reducer(state, action); // Reducer updates the state
+        subscribers.forEach(subscriber => subscriber()) // Notify all subscribers of update to state
     }
 }
 
